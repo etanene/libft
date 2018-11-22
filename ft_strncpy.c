a788cc9786717eb5ac5f130ec6477351b56bc197
Arnbjorn Falmer archer <afalmer-@e4r14p2.21-school.ru>
2018-11-22 16:10:36 +0300

    strncpy added

---
+++ ft_strncpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afalmer- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/22 15:26:52 by afalmer-          #+#    #+#             */
+/*   Updated: 2018/11/22 15:26:53 by afalmer-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+char	*ft_strncpy(char *dst, const char *src, size_t len)
+{
+	char	*new_dst;
+
+	new_dst = dst;
+	if (len)
+	{
+		while (*src && --len)
+			*new_dst++ = *src++;
+		*new_dst++ = *src++;
+		if (len)
+			*new_dst = '\0';
+	}
+	return (dst);
+}
