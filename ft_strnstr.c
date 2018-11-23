376959d85737bbdb8fda4b7c75ceea5b464fd5aa
Arnbjorn Falmer archer <afalmer-@e4r14p2.21-school.ru>
2018-11-23 17:53:39 +0300

    strnstr add, strstr correct

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: afalmer- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/23 12:30:40 by afalmer-          #+#    #+#             */
-/*   Updated: 2018/11/23 12:30:41 by afalmer-         ###   ########.fr       */
+/*   Created: 2018/11/23 15:33:24 by afalmer-          #+#    #+#             */
+/*   Updated: 2018/11/23 15:33:25 by afalmer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strstr(const char *haystack, const char *needle)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	char	*res_hs;
 
 	if (!*needle)
 		return ((char*)haystack);
-	while (*haystack != *needle)
+	while (*haystack != *needle && len--)
 		haystack++;
-	res_hs = (char*)haystack;
-	while (*haystack++ == *needle++)
+	res_hs = (char*)haystack;	
+	while (*haystack++ == *needle++ && len--)
 		;
 	if (!*(needle - 1))
 		return (res_hs);
