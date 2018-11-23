10cab0e6e47c600858cbaff667770b1f327c9ec2
Arnbjorn Falmer archer <afalmer-@e4r14p2.21-school.ru>
2018-11-23 17:21:20 +0300

    strstr correct

--- ft_strstr.c
+++ ft_strstr.c
@@ -15,21 +15,16 @@
 
 char	*ft_strstr(const char *haystack, const char *needle)
 {
-	char	*new_hs;
-	char	*new_needle;
 	char	*res_hs;
 
-	new_hs = (char*)haystack;
-	new_needle = (char*)needle;
-	res_hs = (char*)haystack;
 	if (!*needle)
-		return (res_hs);
-	while (*new_hs != *new_needle)
-		new_hs++;
-	res_hs = new_hs;
-	while (*new_hs++ == *new_needle++)
+		return ((char*)haystack);
+	while (*haystack != *needle)
+		haystack++;
+	res_hs = (char*)haystack;
+	while (*haystack++ == *needle++)
 		;
-	if (!*(new_needle - 1))
+	if (!*(needle - 1))
 		return (res_hs);
 	return (NULL);
 }
