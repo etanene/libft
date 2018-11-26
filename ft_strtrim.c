e275ec899beb348456122c2afe172ffdc3bd0375
Arnbjorn Falmer archer <afalmer-@e4r14p2.21-school.ru>
2018-11-26 18:11:39 +0300

    strtrim add

---
+++ ft_strtrim.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afalmer- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/26 17:19:32 by afalmer-          #+#    #+#             */
+/*   Updated: 2018/11/26 17:19:33 by afalmer-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s)
+{
+	char	*res;
+	char	*s_end;
+
+	s_end = (char*)s + ft_strlen(s) - 1;
+	while (*s == ' ' || *s == '\n' || *s == '\t')
+		s++;
+	while (*s_end == ' ' || *s_end == '\n' || *s_end == '\t')
+		s_end--;
+	if ((res = ft_strnew(s_end - s + 1)) == NULL)
+		return (NULL);
+	res = ft_strncpy(res, s, s_end - s + 1);
+	return (res);
+}
