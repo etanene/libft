28c654ef29b6f0e745eb4d46ccf0c89f4bbc8c13
Arnbjorn Falmer archer <afalmer-@e4r14p2.21-school.ru>
2018-11-26 14:07:03 +0300

    strmapi add

---
+++ ft_strmapi.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afalmer- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/26 13:44:38 by afalmer-          #+#    #+#             */
+/*   Updated: 2018/11/26 13:44:39 by afalmer-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*res;
+	char			*new_res;
+	unsigned int	i;
+
+	i = 0;
+	res = ft_strnew(ft_strlen(s));
+	new_res = res;
+	while (*s)
+		*new_res++ = f(i++, *s++);
+	return (res);
+}
