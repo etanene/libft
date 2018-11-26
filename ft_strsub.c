4b263ae6537ab13dfe317db7cdd04fa73a3dcac3
Arnbjorn Falmer archer <afalmer-@e4r14p2.21-school.ru>
2018-11-26 15:02:35 +0300

    strsub add

---
+++ ft_strsub.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afalmer- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/26 14:29:37 by afalmer-          #+#    #+#             */
+/*   Updated: 2018/11/26 14:29:38 by afalmer-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*res;
+
+	res = ft_strnew(len);
+	if (res != NULL)
+		ft_strncpy(res, s + start, len);
+	return (res);
+}
